b6c89f2fb8c31ecf180ad3491e8b589ed4540f33
redoy-kumar <redoykumar.cse50@gmail.com>
2025-09-16 13:43:32 +0600

    backend section of user role done

---
+++ controllers/updateUser.js
@@ -0,0 +1,36 @@
+import userModel from '../models/userModel.js'
+
+async function updateUser(req,res) {
+    try{
+        const sessionUser = req.userId
+        const {userId,email,name,role} = req.body
+        const payload = {
+            ...(email &&  {email: email}),
+            ...(name &&  {name: name}),
+            ...(role &&  {role: role}),
+        }
+
+        const user = await userModel.findById(sessionUser)
+
+        console.log("User Role: ", user.role);
+
+        const updateUser = await userModel.findByIdAndUpdate(userId,payload)
+
+        res.json({
+            message: "User details updated",
+            data: updateUser,
+            error: false,
+            success: true
+        })
+
+    }catch(error){
+        res.status(400).json({
+            message: error.message || error,
+            error: true,
+            success: false
+        });
+    }
+}
+
+
+export default updateUser;
--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ import userDetailsController from "../controllers/userDetails.js";
 import authToken from "../middleware/authToken.js";
 import userLogout from "../controllers/userLogout.js";
 import allUsers from "../controllers/allUsers.js";
+import updateUser from "../controllers/updateUser.js";
 
 const router = express.Router();
 
@@ -12,6 +13,7 @@ router.post("/signup", userSignUpController);
 router.post("/signin", userSignInController);
 router.get("/user-details", authToken, userDetailsController);
 router.get("/user-logout",userLogout)
+router.post("/update-user",authToken, updateUser)
 
 
 
